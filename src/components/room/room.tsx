bd3bb37b001f9070663d6f44b1456e37c23f5f20
haru <haruki6180@gmail.com>
2023-09-26 13:20:57 +0900

    命名などリファクタ

--- src/components/room/room.tsx
+++ src/components/room/room.tsx
@@ -8,45 +8,44 @@ import {
   SkyWayStreamFactory,
 } from "@skyway-sdk/room";
 import { FC, useEffect, useRef, useState } from "react";
-
 import { contextOptions, tokenString } from "./const";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { RemoteVideo, StyledVideo, VideoContent, Wrapper } from "./style";
 
 const Room = () => {
   const navigate = useNavigate();
-  const audioContainer = useRef<HTMLDivElement>(null);
-  const localVideo = useRef<HTMLVideoElement>(null);
+  const audioContainerRef = useRef<HTMLDivElement>(null);
+  const localVideoRef = useRef<HTMLVideoElement>(null);
   const [videoSubscriptions, setVideoSubscriptions] = useState<
     RoomSubscription<RemoteVideoStream>[]
   >([]);
   const [searchParams] = useSearchParams();
-  const [roomName, setRoomName] = useState(searchParams.get("roomId") ?? "");
+  const [roomId, setRoomId] = useState(searchParams.get("roomId") ?? "");
 
-  const main = async () => {
+  const init = async () => {
     const context = await SkyWayContext.Create(tokenString, contextOptions);
     const room = await SkyWayRoom.FindOrCreate(context, {
-      name: roomName,
+      name: roomId,
       type: "p2p",
     });
-    const member = await room.join();
+    const me = await room.join();
 
     const { audio, video } =
       await SkyWayStreamFactory.createMicrophoneAudioAndCameraStream();
 
-    if (localVideo.current) {
-      localVideo.current.muted = true;
-      localVideo.current.playsInline = true;
-      video.attach(localVideo.current);
-      await localVideo.current.play();
+    if (localVideoRef.current) {
+      localVideoRef.current.muted = true;
+      localVideoRef.current.playsInline = true;
+      video.attach(localVideoRef.current);
+      await localVideoRef.current.play();
     }
 
-    await member.publish(audio);
-    await member.publish(video);
+    await me.publish(audio);
+    await me.publish(video);
 
-    member.onPublicationSubscribed.add((e) => {
+    me.onPublicationSubscribed.add((e) => {
       if (e.stream.contentType === "audio") {
-        const container = audioContainer.current!;
+        const container = audioContainerRef.current!;
         const audio = document.createElement("audio");
         audio.srcObject = new MediaStream([e.stream.track]);
         audio.play();
@@ -56,9 +55,9 @@ const Room = () => {
         });
       }
     });
-    member.onSubscriptionListChanged.add(() => {
+    me.onSubscriptionListChanged.add(() => {
       setVideoSubscriptions(
-        member.subscriptions.filter(
+        me.subscriptions.filter(
           (subscription): subscription is RoomSubscription<RemoteVideoStream> =>
             subscription.contentType === "video"
         )
@@ -66,12 +65,8 @@ const Room = () => {
     });
 
     const subscribe = async (publication: RoomPublication) => {
-      if (publication.publisher.id !== member.id) {
-        if (publication.contentType === "video") {
-          await member.subscribe(publication);
-        } else {
-          await member.subscribe(publication);
-        }
+      if (publication.publisher.id !== me.id) {
+        await me.subscribe(publication);
       }
     };
     room.onStreamPublished.add(async (e) => {
@@ -79,7 +74,7 @@ const Room = () => {
     });
 
     const leaveButton = document.getElementById("leave") as HTMLButtonElement;
-    leaveButton.addEventListener("click", () => member.leave(), {
+    leaveButton.addEventListener("click", () => me.leave(), {
       once: true,
     });
 
@@ -98,14 +93,9 @@ const Room = () => {
       shareButton.onclick = async () => {
         const displayStream = await navigator.mediaDevices.getDisplayMedia();
         const [displayTrack] = displayStream.getVideoTracks();
-        const stream = new LocalVideoStream(displayTrack, {
-          height: 100,
-          width: 800,
-          stopTrackWhenDisabled: true,
-        });
+        const stream = new LocalVideoStream(displayTrack);
         await share.publish(stream);
         await new Promise((resolve) => setTimeout(resolve, 500));
-
         displayStream.getTracks()[0].addEventListener("ended", async () => {
           share.leave();
         });
@@ -114,28 +104,27 @@ const Room = () => {
   };
 
   useEffect(() => {
-    setRoomName(searchParams.get("roomId") ?? "");
-    main();
+    init();
   }, []);
 
   return (
     <Wrapper>
       <div>
-        <div>room Id: {roomName}</div>
+        <div>room Id: {roomId}</div>
         <button id="leave" onClick={() => navigate("/")}>
           退出
         </button>
-        <button id="share" onClick={() => onShare(roomName)}>
+        <button id="share" onClick={() => onShare(roomId)}>
           共有
         </button>
       </div>
       <VideoContent>
-        <StyledVideo ref={localVideo}></StyledVideo>
+        <StyledVideo ref={localVideoRef}></StyledVideo>
         {videoSubscriptions.map((subscription) => (
           <Video key={subscription.id} subscription={subscription} />
         ))}
       </VideoContent>
-      <div ref={audioContainer} />
+      <div ref={audioContainerRef} />
     </Wrapper>
   );
 };
