67a186d42d145ea83bb52b5f171acccc1ed82d1d
haru <haruki6180@gmail.com>
2023-10-06 15:05:32 +0900

    フォーマット修正

--- src/components/roomLists/roomLists.tsx
+++ src/components/roomLists/roomLists.tsx
@@ -2,19 +2,13 @@
 import { RoomLink, Wrapper } from "./style";
 
 const RoomLists = () => {
-  const roomList = [
-    "dog",
-    "cat",
-    "bird",
-    "tomato"
-  ];
+  const roomList = ["dog", "cat", "bird", "tomato"];
 
   return (
     <Wrapper>
-      {
-        roomList.map(item => <RoomLink href={`room?roomId=${item}`}>{item}</RoomLink>)
-      }
-      
+      {roomList.map((item) => (
+        <RoomLink href={`room?roomId=${item}`}>{item}</RoomLink>
+      ))}
     </Wrapper>
   );
 };
--- src/components/roomLists/style.tsx
+++ src/components/roomLists/style.tsx
@@ -1,20 +1,19 @@
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 20px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
 `;
 
 export const RoomLink = styled.a`
-    color: white;
-    background-color: gray;
-    display: block;
-    height: 30px;
-    padding: 20px;
-    border-radius: 20px;
-    text-decoration: none;
+  color: white;
+  background-color: gray;
+  display: block;
+  height: 30px;
+  padding: 20px;
+  border-radius: 20px;
+  text-decoration: none;
 `;
